8a59e209293276e868242858a985cf90bef894b9
eyepathSamurAI <incompleteness1357@gmail.com>
2024-12-01 10:53:51 -0600

    added new puzzle

--- ocaml/aoc/src/2023/day2/puzzle.ml
+++ ocaml/aoc/src/2023/day2/puzzle.ml
@@ -1,1 +1,13 @@
-let () = print_endline "hello world"
+open Core
+
+(* let rec drop n theList =
+  match theList with
+  | [] -> []
+  | _ :: tail when n > 0 -> drop (n-1) tail
+  | _ -> theList *)
+
+let rec drop n lst =
+  match lst with
+  | [] -> [] (* If the list is empty, return an empty list *)
+  | _ :: tl -> if n > 0 then drop (n-1) tl else lst
+
